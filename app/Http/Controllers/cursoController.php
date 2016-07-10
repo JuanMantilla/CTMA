6760340f87627cc4864f40af76b37f315d422a8d
JuanMantilla <t00024526@utbvirtual.edu.co>
2016-07-10 05:25:24 -0500

    Update cursoController.php
    Añadidos comentarios

--- app/Http/Controllers/cursoController.php
+++ app/Http/Controllers/cursoController.php
@@ -12,51 +12,60 @@ class cursoController extends Controller
 {
     public function index()
     {
-        return view('Cursos/curso');
+        //retorna la vista de registrar un curso
+        return view('Cursos/registrarCurso');
     }
 
     //Función que permite guardar un curso
     public function store(Request $request)
     {
-        $fecha=$request->fechaInicial;
-        $año= substr($fecha, -16, 4);
-        $mes= substr($fecha, -11, 2);
-        $mesInt = (int)$mes;
-        $añoInt=(int)$año;
+        //Busco el profesor con el código del profesor que se le quiere asignar al curso a registrar
         $profesor = DB::table('profesores')
             ->where('codigo', '=', $request->codigoProfesor)
             ->get();
-        foreach ($profesor as $profe){
-            $id=$profe->id;
-        }
+        //si se encuentra el profesor, continúa a crear y guardar el curso en la BD
         if($profesor){
+            //maneja lo referente a la fecha del curso
+            $fecha=$request->fechaInicial;
+            $año= substr($fecha, -16, 4);
+            $mes= substr($fecha, -11, 2);
+            $mesInt = (int)$mes;
+            $añoInt=(int)$año;
+            foreach ($profesor as $profe){
+                $id=$profe->id;
+            }
+            if($mesInt>6){
+                $periodo=02;
+            }else{
+                $periodo=01;
+            }
+            //crea el curso y lo guarda en la BD
+            $curso = new Curso([
+                'nombre' => $request->nombre,
+                'descripcion' => $request->descripcion,
+                'periodo' => $periodo,
+                'año' => $añoInt,
+                'fecha_inicial' => $request->fechaInicial,
+                'profesor_id' => $id,
+            ]);
+            $curso->save();
+            //muestra el mensaje satisfactorio y retorna al index
             $message = "Se creó el curso satisfactoriamente";
             echo "<script type='text/javascript'>alert('$message');</script>";
+            return view('/index');
         }
+        //si no se encontró el profesor con el código dado, no se puede guardar el curso, acá se muestra ese mensaje 
+        //como alerta y se devuelve a la vista de registrar un curso
         else{
             $message = "El código del profesor que ingresaste no existe, por lo tanto, no se pudo crear el curso";
             echo "<script type='text/javascript'>alert('$message');</script>";
-            return view('Cursos/curso');
-        };
-        if($mesInt>6){
-            $periodo=02;
-        }else{
-            $periodo=01;
+            return view('Cursos/registrarCurso');
         }
-        $curso = new Curso([
-            'nombre' => $request->nombre,
-            'descripcion' => $request->descripcion,
-            'periodo' => $periodo,
-            'año' => $añoInt,
-            'fecha_inicial' => $request->fechaInicial,
-            'profesor_id' => $id,
-        ]);
-        $curso->save();
-        return view('/index');
     }
 
     public function verCursos()
     {
+        //extraigo todos los cursos de la BD, los guarda en la variable cursos y retorna la vista de cursos
         $cursos= Curso::all();
         return view('Cursos/cursos')->with('cursos', $cursos);
     }
